c01a01f25e4a2e401208ed85ae5c3eddb9103dcc
SoyPoloyo <47046128+SoyPoloyo@users.noreply.github.com>
2020-03-26 00:43:01 -0600

    asd

--- APP_Pruebas/public/js/especializado.js
+++ APP_Pruebas/public/js/especializado.js
@@ -1,1 +1,7 @@
-validacionformulario
+function enviar() {
+    var data = {
+        nombre: document.getElementById('nombre').value,
+        edad: document.getElementById('apellido').value,
+        carrera: document.getElementById('correo').value
+    };
+console.log({data})
