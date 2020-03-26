434a783fbcdecccd5bc1b538aeb7c75fe0fa1640
SoyPoloyo <47046128+SoyPoloyo@users.noreply.github.com>
2020-03-26 10:48:48 -0600

    ...

--- APP_Pruebas/public/js/especializado.js
+++ APP_Pruebas/public/js/especializado.js
@@ -1,8 +1,31 @@
-function enviar() {
-    var data = {
-        nombre: document.getElementById('nombre').value,
-        edad: document.getElementById('apellido').value,
-        carrera: document.getElementById('correo').value
+function enviar () {
+    console.log('probando');
+    var nombre, apellido, edad;
+    nombre = document.getElementById('nombre').value;
+     apellido = document.getElementById('apellido').value;
+     edad = document.getElementById('edad').value;
+   
+    var valores = {
+        nombre: nombre,
+        apellido: apellido,
+        edad: edad,
     };
-      }
-console.log({data});
+    console.log(valores);
+    
+    fetch("http://localhost:5252/registroEspecializado/insertar", {
+        body: JSON.stringify(valores),
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function(data){
+        return data.json()
+    })
+    .then(function(res){
+        console.log(res)
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+}
