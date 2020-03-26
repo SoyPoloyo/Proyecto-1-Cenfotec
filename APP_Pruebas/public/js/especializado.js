7bfa70569358286cc47fc021b64ecea5a8edb29a
SoyPoloyo <47046128+SoyPoloyo@users.noreply.github.com>
2020-03-26 16:57:32 -0600

    asd

--- APP_Pruebas/public/js/especializado.js
+++ APP_Pruebas/public/js/especializado.js
@@ -1,71 +1,56 @@
-function validarNombre(){
-    var validacion = true;
-    if(document.getElementById("nombre").value==""){
-        validacion=false;
-        alert("Ingrese su nombre");
-    }
-    return validacion;
-}
-function validarApellido(){
-    var validacion = true;
-    if(document.getElementById("apellido").value==""){
-        validacion=false;
-        alert("Ingrese su apellido");
-    }
-    return validacion;
-}
-function validarEdad(){
-    var validacion = true;
-    if(document.getElementById("edad").value==""){
-        validacion=false;
-        alert("Ingrese su edad");
-    }
-    return validacion;
-}
-function validarCorreo(){
-    var validacion = true;
-    if(document.getElementById("correo").value==""){
-        validacion=false;
-        alert("Ingrese su correo");
-    }
-    return validacion;
-}
-function validatipoCedula(){
-    var validacion = true;
-    if(document.getElementById("tipoCedula").value==""){
-        validacion=false;
-        alert("Ingrese su tipo de Cedula");
-    }
-    return validacion;
-}
-function validarCedula(){
-    var validacion = true;
-    if(document.getElementById("cedula").value==""){
-        validacion=false;
-        alert("Ingrese su cedula");
-    }
-    return validacion;
-}
-function validarRegistro(){
-    if(validarNombre()==true&&validarApellido()==true&&validarEdad()==true&&validarCorreo()==true
-    &&validartipoCedula()==true&&validarCedula()==true)
-    {
-        var data={
-            nombre: document.getElementById("nombre").value,
-            apellido: document.getElementById("apelido").value,
-            edad: document.getElementById("edad").value,
-            correo: document.getElementById("correo").value,
-            tipoCedula: document.getElementById("tipoCedula").value,
-            cedula: document.getElementById("tipoCedula").value
-        };
-        alert("Formulario enviado exitosamente")
-        console.log(data);
-        fetch('/api')
-    }
 
     /************************************************************
-     aca viene el codigo que funciona por aquello
+     Codigo Validacion de Elio
      ****************************************************/
+
+    function enviar () {   
+        let datos = ['nombre', 'apellido', 'edad'];
+        let valoresPrueba = {}; 
+        let aprobado = false;
+    
+        for (var dato of datos) {         
+            valoresPrueba[dato] = document.getElementById(dato).value;;
+        }
+        for (let i in valoresPrueba) {
+           if(valoresPrueba[i] == ""){
+               console.log('vacio');
+               alert("Debe llenar todos los campos");
+               aprobado= false;
+               break
+           }else{
+         (console.log("mierda funciono"))
+         aprobado = true
+        }       
+        }
+
+        if(aprobado){
+              fetch("http://localhost:5252/registroEspecializado/insertar", {
+                        body: JSON.stringify(valoresPrueba),
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then(function(data){
+                        return data.json()
+                    })
+                    .then(function(res){
+                        console.log(res)
+                    })
+                    .catch(function(err){
+                        console.log(err)
+                    })
+        }
+        alert("Se ha creado su cuenta con exito")    
+    }
+     
+
+    /************************************************************
+     Codigo normal - No contiene Validacion*
+     ****************************************************/
+
+     /*
+
 function enviar () {
     console.log('probando');
     var nombre, apellido, edad;
@@ -97,3 +82,5 @@ function enviar () {
         console.log(err)
     })
 }
+
+*/
