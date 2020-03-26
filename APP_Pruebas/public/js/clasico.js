af7f1936655185e8d764219379ad0cd5b88ff813
SoyPoloyo <47046128+SoyPoloyo@users.noreply.github.com>
2020-03-26 11:01:16 -0600

    avance

--- APP_Pruebas/public/js/clasico.js
+++ APP_Pruebas/public/js/clasico.js
@@ -1,1 +1,31 @@
-sd
+function enviar () {
+    console.log('probando');
+    var nombre, apellido, edad;
+    nombre = document.getElementById('nombre').value;
+     apellido = document.getElementById('apellido').value;
+     edad = document.getElementById('edad').value;
+   
+    var valores = {
+        nombre: nombre,
+        apellido: apellido,
+        edad: edad,
+    };
+    console.log(valores);
+    
+    fetch("http://localhost:5252/registroClasico/insertar", {
+        body: JSON.stringify(valores),
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function(data){
+        return data.json()
+    })
+    .then(function(res){
+        console.log(res)
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+}
