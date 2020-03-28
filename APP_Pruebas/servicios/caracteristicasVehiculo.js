d82eecaea8e77e9745ef413b4ddb8c66e7f456c9
KuromiDL <kuromidarklord@gmail.com>
2020-03-28 00:22:12 -0600

    HTML, CSS Y JS annadidos
    HTML, CSS Y JS annadidos. aun falta la validacion

--- APP_Pruebas/servicios/caracteristicasVehiculo.js
+++ APP_Pruebas/servicios/caracteristicasVehiculo.js
@@ -1,5 +1,4 @@
-
-/*var express = require('express');
+var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var CaracteristicaVehiculo = require('../schemas/caracteristicasVehiculo');
