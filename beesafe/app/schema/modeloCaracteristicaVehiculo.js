ec390253862a3189bcd3e30a3b33394bf5a6dc22
KuromiDL <kuromidarklord@gmail.com>
2020-03-29 03:07:06 -0600

    inicio de la validacion.
    Inicio de la validacion. Despierto en 4 horas para terminarla

--- beesafe/app/schema/modeloCaracteristicaVehiculo.js
+++ beesafe/app/schema/modeloCaracteristicaVehiculo.js
@@ -2,8 +2,10 @@ var mongoose = require("mongoose");
 
 var caracteristicasVehiculoSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  nombre: String,
-  descripcion: String,
+  nombre: { Type: String, required: true},
+  required: [true, "No se ha colocado el nombre de la caracterstica"]
 });
-
+app.post("/register", function(req, res){
+  const newUser
+})
 module.exports = mongoose.model( "caracteristicasVehiculo", caracteristicasVehiculoSchema ,"caracteristicasVehiculos");
