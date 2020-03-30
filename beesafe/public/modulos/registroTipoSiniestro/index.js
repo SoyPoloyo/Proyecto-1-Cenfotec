8036afac6d026b52bf78332e5ca221dbbcb47ce7
franciscoraudezr <60897360+franciscoraudezr@users.noreply.github.com>
2020-03-29 22:25:04 -1100

    FUNCIONÓ TODO !!!!!!!!!!!!!!!!!!!

--- beesafe/public/modulos/registroTipoSiniestro/index.js
+++ beesafe/public/modulos/registroTipoSiniestro/index.js
@@ -1,5 +1,5 @@
 
-function registro() {
+/*function registro() {
     var aprobado = false;
     var nombre = document.getElementById('nombre').value;
     var descripcion = document.getElementById('descripcion').value;
@@ -25,4 +25,39 @@ function registro() {
             .catch(error => console.log('Error:', error))
             .then(response => console.log('Success:', response));
     }
+}*/
+
+function registro(){
+    let datos = ["nombre","descripcion","image"];
+    let valores = {};
+    let aprobado = false;
+
+    for (let dato of datos) {
+        valores[dato] = document.getElementById(dato).value;
+    }
+    for (let i in valores) {
+        if (valores[i] == "" ) {
+            alert("Debe llenar todos los campos de manera correcta");
+            aprobado = false;
+            break;
+        } else {
+            aprobado = true;
+        }
+    }
+    if(aprobado){
+        const formData = new FormData();
+        formData.append('nombre', document.getElementById('nombre').value);
+        formData.append('descripcion', document.getElementById('descripcion').value);
+        formData.append('image', document.getElementById('image').files[0]);
+
+        fetch('/registroTipoSiniestro/insertar', {
+            method: 'POST',
+            body: formData, 
+        }).then(res => res.json())
+            .catch(error => console.log('Error:', error))
+            .then(response => console.log('Success:', response));
+    
+    }
+
 }
+
