7159bbe35a79469077d81936a3fc72b72de27503
franciscoraudezr <60897360+franciscoraudezr@users.noreply.github.com>
2020-03-30 01:13:37 -1100

    askncslakn
    aflsdfañsdlka

--- beesafe/app/servicios/registroTipoSiniestro.js
+++ beesafe/app/servicios/registroTipoSiniestro.js
@@ -3,14 +3,14 @@ const router = Router();
 
 const TipoSiniestro = require('../schema/modeloTipoSiniestro');
 
-/* router.get('/', async (req, res) => {
+router.get('/ver', async (req, res) => {
     const tipoSiniestros = await TipoSiniestro.find();
     res.json(tipoSiniestros);
-}); */
+});
 
 router.post('/insertar', async (req, res) => {
     const { nombre, descripcion } = req.body;
-    const imagen = '/uploads/' + req.file.filename;
+    const imagen = '/assets/uploads/' + req.file.filename;
     const newTipoSiniestro = new TipoSiniestro({ nombre, descripcion, imagen });
     console.log(newTipoSiniestro);
     await newTipoSiniestro.save();
