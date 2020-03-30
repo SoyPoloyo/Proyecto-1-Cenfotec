5fb9c7495799cb0dcab31c12816bc8c89017aa20
franciscoraudezr <60897360+franciscoraudezr@users.noreply.github.com>
2020-03-29 15:06:16 -1100

    lkjlkjl
    lkjlkjlkj

--- beesafe/app/index.js
+++ beesafe/app/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require("path");
 var app = express();
 var mongoose = require('mongoose');
-// var multer = require('multer'); //para cargar imagenes al servidor se necesita instalar
+var multer = require('multer'); 
 
 mongoose.connect('mongodb+srv://admin:admin@workapes-9hxh3.mongodb.net/pruebas?retryWrites=true&w=majority',
  {useNewUrlParser:true, useUnifiedTopology:true})
@@ -11,15 +11,17 @@ mongoose.connect('mongodb+srv://admin:admin@workapes-9hxh3.mongodb.net/pruebas?r
 
 
 
-/* const storage = multer.diskStorage({
-    destination: path.join(__dirname, 'public/uploads'),
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, '../public/assets/uploads'),
     filename(req, file, cb) {
         cb(null, new Date().getTime() + path.extname(file.originalname));
     }
 })
 
 app.use(multer({storage}).single('image')); 
-app.use(express.urlencoded({extended: false})); */
+app.use(express.urlencoded({extended: false})); 
+
+
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
