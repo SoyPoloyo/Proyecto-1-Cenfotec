03f365733629bdd250cc77c62d8d91f46b2e881b
SoyPoloyo <47046128+SoyPoloyo@users.noreply.github.com>
2020-03-30 14:52:39 -0600

    ...

--- beesafe/public/modulos/sesionUsuarioAdmin/index.js
+++ beesafe/public/modulos/sesionUsuarioAdmin/index.js
@@ -1,47 +1,44 @@
-
 /************************************************************
      codigo para subir la imagen
      ****************************************************/
 
-    function subirImagen() {
-    
-        console.dir(document.getElementById('iconoAgregado'));
-          var archivo = document.getElementById("botonAgregarImagen").files[0];
-          var reader = new FileReader();
-          if (botonAgregarImagen) {
-            reader.readAsDataURL(archivo);
-            reader.onloadend = function () {
-              document.getElementById("iconoAgregado").src = reader.result;
-              document.getElementById("iconoAgregado").classList.add('imagen_subida');
-          }
-        }
-
-        document.getElementById('muestraIcono').remove();
-      }
-
-      function mostrar(){
-
-        var perro;
-    
-        fetch('/registroTipoSiniestro/mostrar', {
-            body: JSON.stringify(),
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json'
-            } 
-        }).then(res => res.json())
-            .catch(error => console.log('Error:', error))
-            .then(response =>   console.log('Success:', response));
-    
-    
-            console.log('esto es abajo');
-            console.log(perro);
-    }
-
-
-    function cambio(){
-
-        document.getElementById('siniestro').setAttribute("hidden", true);
-
-  
+function subirImagen() {
+  console.dir(document.getElementById("iconoAgregado"));
+  var archivo = document.getElementById("botonAgregarImagen").files[0];
+  var reader = new FileReader();
+  if (botonAgregarImagen) {
+    reader.readAsDataURL(archivo);
+    reader.onloadend = function() {
+      document.getElementById("iconoAgregado").src = reader.result;
+      document.getElementById("iconoAgregado").classList.add("imagen_subida");
+    };
   }
+
+  document.getElementById("muestraIcono").remove();
+}
+
+function mostrar() {
+  var perro;
+
+  fetch("/registroTipoSiniestro/mostrar", {
+    body: JSON.stringify(),
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .catch(error => console.log("Error:", error))
+    .then(response => console.log("Success:", response));
+
+  console.log("esto es abajo");
+  console.log(perro);
+}
+
+function siniestros() {
+  document.getElementById("siniestro").removeAttribute("hidden");
+}
+
+function caracteristicasVehiculo() {
+  document.getElementById("siniestro").setAttribute("hidden", true);
+}
