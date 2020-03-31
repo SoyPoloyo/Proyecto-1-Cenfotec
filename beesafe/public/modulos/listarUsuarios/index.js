6bc9b24882034757cc13df6f7a1e85a06dca6a1e
SoyPoloyo <47046128+SoyPoloyo@users.noreply.github.com>
2020-03-30 21:04:58 -0600

    Update index.js

--- beesafe/public/modulos/listarUsuarios/index.js
+++ beesafe/public/modulos/listarUsuarios/index.js
@@ -1,38 +1,27 @@
+function probando() {
+  fetch("http://localhost:5252/registroUsuarioClasico/recibir", {
+    body: JSON.stringify(),
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(function(data) {
+      return data.json();
+    })
+    .then(function(res) {
+      console.log();
 
-var existentes = [];
-function  probando (){
-    
-    fetch("http://localhost:5252/registroUsuarioClasico/recibir", {
-     body: JSON.stringify(),
-     method: "GET",
-     headers: {
-       "Content-Type": "application/json"
-     }
-   })   
-     .then(function(data) {
-       return data.json();
-     })
-     .then(function(res) {
-       console.log();
+      console.log("segundo then");
 
-      
-       console.log('segundo then');
-      for (let i in res) {
-        if (res[i]) {
-           existentes = push.res[i];
-          
-        }
-      }
-      console.log(existentes);
-       document.getElementById("prueba").innerHTML = res[2].tipoUsuario
-       
-     })
-     .catch(function(err) {
-       console.log(err);
-     });
-    
-       console.log(existentes);
-   
-  }
-   probando();
- 
+      console.log(res.length)
+      console.log(res[1])
+     
+      document.getElementById("prueba").innerHTML = res[2].tipoUsuario;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+
+}
+probando();
