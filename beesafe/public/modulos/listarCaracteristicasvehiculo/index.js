1562d233db5be9827cedb28045b60cc42e5ecbf4
KuromiDL <kuromidarklord@gmail.com>
2020-04-01 17:19:20 -0600

    edidicon del html de admin

---
+++ beesafe/public/modulos/listarCaracteristicasvehiculo/index.js
@@ -0,0 +1,35 @@
+function probando() {
+  fetch("http://localhost:5252/registroCaracteristicasVehiculo/recibir", {
+    body: JSON.stringify(),
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(function(data) {
+      return data.json();
+    })
+    .then(function(res) {
+      console.log();
+
+      console.log("segundo then");
+
+      console.log(res.length)
+      console.log(res[1])
+      let objeto = {}
+      objeto = res;
+      console.log(objeto.length)
+     
+      for (let i = 0; i <= objeto.length; i++) {
+       
+        document.getElementById("prueba").innerHTML = objeto[i].nombre;     
+      }
+
+
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+
+}
+probando();
