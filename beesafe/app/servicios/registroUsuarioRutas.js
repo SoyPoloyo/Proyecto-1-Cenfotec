ac3a35d20789b8b9a008209d716b4fca75c290ba
SoyPoloyo <47046128+SoyPoloyo@users.noreply.github.com>
2020-04-08 17:38:45 -0600

    Update registroUsuarioRutas.js

--- beesafe/app/servicios/registroUsuarioRutas.js
+++ beesafe/app/servicios/registroUsuarioRutas.js
@@ -52,20 +52,5 @@ router.post("/insertar", async function(req, res) {
 });
 
 
-router.get("/recibir", function(req, res) { 
-
-  Usuario.find({})
-    .exec()
-
-    .then(function(result) {
-      res.json(result);
-    })
-
-    .catch(function(err) {
-      console.log(err);
-    });
-
-
-});
 
 module.exports = router;
