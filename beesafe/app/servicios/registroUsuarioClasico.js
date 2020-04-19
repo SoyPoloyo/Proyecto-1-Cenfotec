f6cecabbbdaa85804d91e6ec526e3c5264943265
SoyPoloyo <47046128+SoyPoloyo@users.noreply.github.com>
2020-04-19 06:12:16 -0600

    correo listo

--- beesafe/app/servicios/registroUsuarioClasico.js
+++ beesafe/app/servicios/registroUsuarioClasico.js
@@ -39,33 +39,55 @@ router.post("/insertar", async function (req, res) {
     },
   });
 
-  contentHTML = `
 
-
-
-
-  <html>
+ 
+contentHTML = `<html>
   <head>
-      <style>button:hover{cursor: pointer; 
-          background-color:#cc5d01 ;
+  <link href="https://fonts.googleapis.com/css?family=Baloo|Baloo+2&display=swap" rel="stylesheet">
+      
+      <style>
+          .cuerpo {
+              background: #f3f3f3;
+              font-family: "Baloo 2", cursive;
           }
-      </style> 
-  </head> 
-  <body style="text-align: center;
-               background-image:url('fondoabeja.png'); 
-               background-repeat: no-repeat ; 
-               background-size: cover; "> 
-      <img src='../../public/assets/img/BEESAFE.png' width='30%'> 
-      <h1>BeeSafe</h1> 
-      ${correo}
-      <p>Gracias por registrarse en nuestra aplicación</p>
-      <p>Le recordamos cambiar su contraseña una vez ingrese a la aplicación</p>
-      <input type="button" onclick="window.location.href='http://localhost:5252/index.html';" value="Iniciar Sesion" />
+  
+          .container {
+              margin: 0 auto;
+              background: #fff;
+              width: 500px;
+              text-align: center;
+              padding: 10px;
+          }
+  
+          .boton {
+              background: #cc5d01;
+              color: #2f2319;
+              display: block;
+              padding: 15px;
+              text-decoration: none;
+              width: 50%;
+              margin: 0 auto;
+              font-weight: bolder;
+          }
+      </style>
+  </head>
+  
+  <body class="cuerpo">
+      <div class="container">
+     
+          <h1>Bienvenido a BeeSafe</h1>
+          <h2>Su reporte de siniestros</h2>
+  
+          <p>Saludos ${usuarioNuevo.nombre} ${usuarioNuevo.apellido} ahora puedes ingresar a la aplicación de siniestros viales.</p>
+          <p>El correo electrónico asociado es: ${usuarioNuevo.correo}</p>
+          <p>Su contraseña temporal es: ${usuarioNuevo.password}</p>
+          <p>Para ingresar de clikc al botón<p>
+                  <a target="blanck" href="http://localhost:5252/index.html" class="boton">Ingresar a la app </a>
+      </div>
+  
   </body>
- </html>
-
-
-`;
+  
+  </html> `;
 
   const infoCorreo = await transporter.sendMail({
     from: '"BeeSafe 🐝" <beesafe@alineacr.com>', // sender address
