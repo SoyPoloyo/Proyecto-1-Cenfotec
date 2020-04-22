19f7de494b760c51cc6b730a21a6c8dd712dd9b0
JDSALASBRENES <60898316+JDSALASBRENES@users.noreply.github.com>
2020-04-22 00:39:25 -0600

    siniestroactualFuncional

--- beesafe/app/servicios/reportarSiniestro.js
+++ beesafe/app/servicios/reportarSiniestro.js
@@ -10,20 +10,25 @@ router.post("/insertar", async function(req, res) {
 
 
   
+  console.log("x");
   var siniestroNuevo = new siniestro({
     _id: new mongoose.Types.ObjectId(),
     ruta: req.body.ruta,
     asistencia: req.body.asistencia,
     tipoSiniestro: req.body.tipoSiniestro,
-    descripcion: req.body.descripcion
+    descripcion: req.body.descripcion,
+    longitude: req.body.longitude,
+    latitude: req.body.latitude
    
   });
-
+  console.log("y");
   siniestroNuevo.save()
     .then(function(resultado) {
       res.json(resultado);
+      console.log("result");
     })
     .catch(function(error) {
+      console.log("error");
       console.log(error);
     });
 
