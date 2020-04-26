2b3a14a0b4c2df78fe42c638965e099ad39b5e57
SoyPoloyo <47046128+SoyPoloyo@users.noreply.github.com>
2020-04-25 19:00:43 -0600

    vehiculos

--- beesafe/app/schema/modeloVehiculo.js
+++ beesafe/app/schema/modeloVehiculo.js
@@ -4,8 +4,8 @@ var mongoose = require("mongoose");
 var vehiculoSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   placaVehi: String,
-  modelo: String,
-  anno: Number,
+ 
+ 
   marca_vehiculo: String,
   identificador: String,
   chofer:String,
