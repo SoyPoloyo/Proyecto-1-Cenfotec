59d8489e26b6424b1c5e8b90bcdff6eb58ef268a
franciscoraudezr <60897360+franciscoraudezr@users.noreply.github.com>
2020-04-27 00:42:07 -1100

    asistencias clasico especializado listar

--- beesafe/app/servicios/listarSiniestroAsistencia.js
+++ beesafe/app/servicios/listarSiniestroAsistencia.js
@@ -5,20 +5,51 @@ var mongoose = require("mongoose");
 const SiniestroAsistencia = require('../schema/modeloSiniestroAsistencia')
 
 
-router.get("/recibir", function(req, res) { 
+router.get("/recibir", function (req, res) {
 
-    SiniestroAsistencia.find()
-      .exec()
-  
-      .then(function(result) {
-        res.json(result);
-      })
-  
-      .catch(function(err) {
-        console.log(err);
-      });
-  
-  
-  });
-  
-  module.exports = router;
+  SiniestroAsistencia.find()
+    .exec()
+
+    .then(function (result) {
+      res.json(result);
+    })
+
+    .catch(function (err) {
+      console.log(err);
+    });
+
+
+});
+
+
+
+router.post("/recibirClasico", function (req, res) {
+
+  usuarioSolicitante = req.body.usuarioSolicitante
+
+  SiniestroAsistencia
+    .find({ usuarioSolicitante: usuarioSolicitante })
+    .then(function (resultado) {
+      res.json(resultado);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+router.post("/recibirEspecializado", function (req, res) {
+
+  usuarioEspecializado = req.body.usuarioEspecializado
+
+  SiniestroAsistencia
+    .find({ usuarioEspecializado: usuarioEspecializado })
+    .then(function (resultado) {
+      res.json(resultado);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+});
+
+module.exports = router;
